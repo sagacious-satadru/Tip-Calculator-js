b0d7ee6d3eeb00d2496e79d08ee16c2e8ebf8934
Satadru Bhowmik <110734942+sagacious-satadru@users.noreply.github.com>
2023-03-23 18:38:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
 /* 
 🌟 APP: Tip Calculator
-
-These are the 3 functions you must use 👇
-=========================================
-calculateBill()
-increasePeople()
-decreasePeople()
-
-These functions are hard coded in the HTML. So, you can't change their names.
-
-These are all the DIV ID's you're gonna need access to 👇
-========================================================
-#1 ID 👉 'billTotalInput' = User input for bill total
-#2 ID 👉 'tipInput' = User input for tip
-#3 ID 👉 'numberOfPeople' = Current number of people you're splitting the bill between
-#4 ID 👉 'perPersonTotal' = Total dollar value owed per person
 */
 
-// Get global access to all inputs / divs here (you'll need them later 😘)
+// We get global access to all inputs / divs here 
 // bill input, tip input, number of people div, and per person total div
 
 const totBill = document.getElementById('billTotalInput');
